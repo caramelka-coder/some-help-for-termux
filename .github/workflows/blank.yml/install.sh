4be6c4e1c59ad84dafdc7487a341faf136128cd3
caramelka-coder <69632606+caramelka-coder@users.noreply.github.com>
2020-08-13 15:30:26 +0300

    install.sh

---
+++ .github/workflows/blank.yml/install.sh
@@ -0,0 +1,6 @@
+clear 
+echo "Hello user! This is script was created by me for help you to install some packages in Termux"
+echo -e "This script will installing:\n- Python3, Python2\n- wget\n- curl\- nano\n- micro
+echo -e "Enjoy!"
+apt install wget curl nano python2 python micro
+echo -e "It's all!"
